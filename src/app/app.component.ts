954b6bfe66306c9d3bcb1ce835b9594c8d5f8c80
Jhon Alexander Castillo Forero <jhon.castillo@rappi.com>
2020-06-01 10:42:51 -0500

    fix dropdown translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -81,6 +81,23 @@ export class AppComponent implements OnInit {
 
 
   public formFields: GFormFields = [
+    // dropdown input
+    new GDropdownField({
+      model: 'dropdown',
+      label: 'Dropdown',
+      placeholder: 'Dropdown placeholder',
+      externalOptions: true,
+      optionValues: [
+        {
+          value: 'yellow',
+          text: 'yellow'
+        },
+        {
+          value: 'red',
+          text: 'red'
+        }
+      ]
+    }),
     // time input
     new GTimeField({
       model: 'time',
@@ -527,11 +544,11 @@ export class AppComponent implements OnInit {
       ...this.formOptions.fieldValues,
       dropdown: [
         {
-          value: 'hola',
+          value: 'HOLA_KEY',
           text: 'hola'
         },
         {
-          value: 'chao',
+          value: 'CHAO_KEY',
           text: 'chao'
         }
       ]
@@ -561,7 +578,8 @@ export class AppComponent implements OnInit {
         lng: -74.1126673,
         city: 'Bogotá',
         country: 'Colombia'
-      }
+      },
+      dropdown: 'CHAO_KEY'
     };
     this.formFields = this.gsFormService.patchFormValues(this.formFields, toPatchValues);
   }
