15b1c0e52c334bb25dd2e20c28b0e0086d29f13b
Gustavo Santamaria <gustavo.santamaria@rappi.com>
2020-06-15 13:32:37 -0500

    password field

--- projects/gs-forms/src/lib/gs-fields/password-input/password-input.component.ts
+++ projects/gs-forms/src/lib/gs-fields/password-input/password-input.component.ts
@@ -1,19 +1,16 @@
 import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
 import { GPasswordField } from '../../gs-forms.widgets';
-import { GFieldValidatorType } from '../../gs-forms.enums';
+import { RppGenericFieldComponent } from '../_generic-field/_generic-field.component';
 
 @Component({
   selector: 'gs-password-input',
   templateUrl: './password-input.component.html',
   styleUrls: ['./password-input.component.sass']
 })
-export class GsPasswordInputComponent {
+export class GsPasswordInputComponent extends RppGenericFieldComponent {
   @Input() public field: GPasswordField;
-  @Input() public formGroup: FormGroup;
 
   public toggle: boolean;
-  public fieldValidatorType = GFieldValidatorType;
 
   public toggleInputType() {
     this.toggle = !this.toggle;
